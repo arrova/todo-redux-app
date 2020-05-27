fd4cce1e4f9e75b93062ac8f891767913dc2e4b9
Arturo Rodríguez <red.cerberus640@gmail.com>
2020-05-27 14:07:05 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoModule } from './todos/todo.module';
+import { environment } from '../environments/environment';
+import { appReducers } from './app.reducer';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    TodoModule,
+    StoreModule.forRoot( appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
